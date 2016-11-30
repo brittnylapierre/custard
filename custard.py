11973f77513640fb3c62bb48395cc1fd7c292df8
l3rittny <brittny.lapierre@gmail.com>
2016-11-30 00:45:22 -0500

    check for mathcing items before add

--- custard.py
+++ custard.py
@@ -29,30 +29,35 @@ class MainWindow(QMainWindow): #QWidget
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
+
+        #Watch for changes in clipboard if change occurs run changeSlot function
+        self.connect(QApplication.clipboard(),SIGNAL("dataChanged()"),self,SLOT("changedSlot()"))
+
+        #Create key listening thread for toggle view and attach signal for communicating to this main window
         self.thread = KeyListener()
         self.connect(self.thread, self.thread.signal, self.handleKey)
-        self.clipboard_history = []
+        
+        #Default values
+        #TODO: find a way to set within gui then pass to key listen thread
         self.set_toggle_on_code = -1
         self.set_toggle_off_code = -1
         self.toggle_code = -1
+
+        self.clipboard_history = [] #might come in handy?
         
         
     def initUI(self):
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('Icon')
-        self.setWindowIcon(QtGui.QIcon('web.png'))        
-        self.show()
-        self.connect(QApplication.clipboard(),SIGNAL("dataChanged()"),self,SLOT("changedSlot()"))
+        self.setWindowIcon(QtGui.QIcon('web.png')) 
         self.grid_layout    = QtGui.QGridLayout()
         self.central_widget = QtGui.QWidget()
         self.list_widget = ListWidget()
-
         self.grid_layout.addWidget(self.list_widget,0,0)
-
         self.setCentralWidget(self.central_widget)
         self.central_widget.setLayout(self.grid_layout)
-
         self.resize(400,400)
+        self.show()
         self.hidden = False
 
     def closeEvent(self, evnt):
@@ -79,42 +84,46 @@ class MainWindow(QMainWindow): #QWidget
         key = event.key()
         print(event, "\nkey: ", key)
 
+        #On enter or return remove the curr selected item from item list and copy to clipboard
         if key == Qt.Key_Return or key == Qt.Key_Enter:
             #Copy to clipboard
             QApplication.clipboard().setText(self.list_widget.currentItem().text())
-            print(self.list_widget.currentItem().text(), " copied to clipboard")
+            print(self.list_widget.currentItem().text(), " removed from list and copied to clipboard")
 
             #Remove from list
             matching_items = self.list_widget.findItems(self.list_widget.currentItem().text(), Qt.MatchExactly)
             for item in matching_items:
                 self.list_widget.takeItem(self.list_widget.row(item))
-                print("removed", item.text())
+                #print("removed", item.text())
 
     @pyqtSlot()
     def changedSlot(self):
-        #print("change")
         if(QApplication.clipboard().mimeData().hasText()):
             clipboard_content = QApplication.clipboard().text()
-            #push to firs]]]t index
-            self.clipboard_history.insert(0, clipboard_content); 
-            print(self.clipboard_history);
+            #Remove from list
 
-            #add clipboard contents to list widget
-            self.list_widget.addItem(clipboard_content);
+            #Check if already in list, not in list if no mathcing items
+            matching_items = self.list_widget.findItems(clipboard_content, Qt.MatchExactly)
+
+            if len(matching_items) == 0:
+                #push to first index
+                self.clipboard_history.insert(0, clipboard_content); 
+                #print(self.clipboard_history);
+
+                #add clipboard contents to list widget
+                self.list_widget.addItem(clipboard_content);
+            else: 
+                #TODO: make item first in list?
+                print("Selected text already in history.")
 
 
 #List widget that stores clipboard history text
 class ListWidget(QListWidget):
     def __init__(self):
         super(ListWidget, self).__init__()
-        #self.currentItemChanged.connect(self.copyToClipboard)
-
-    def copyToClipboard(self, curr, prev):
-        print('Selected: ', str(curr.text()))
-        QApplication.clipboard().setText(str(curr.text()))
 
 
-#will need to multithread global key listener and clipboard chnage detection
+#Will need to multithread global key listener to toggle our program even when it doesnt have keyboard focus
 class KeyListener(QThread):
 
     def __init__(self, parent = None):
@@ -128,7 +137,6 @@ class KeyListener(QThread):
         platform_name = platform.system()
         print "You are using " + platform_name;
         if platform_name == "Linux":
-            #will need to multithread global key listener and clipboard chnage detection
             #Create hookmanager
             self.hookman = pyxhook.HookManager()
 
@@ -143,6 +151,8 @@ class KeyListener(QThread):
 
             #Close the listener when we are done
             #self.hookman.cancel()
+
+        #Windows    
         elif platform_name == "Windows":
             # create a hook manager
             self.hookman = pyHook.HookManager()
@@ -157,7 +167,8 @@ class KeyListener(QThread):
     #This function is called every time a key is presssed
     def linux_kbevent(self, event):
         #print key info
-        print(event.Ascii)
+        #print(event.Ascii)
+
         #Disable watch for toggle on `
         if event.Ascii == 126:
             self.watching = False
